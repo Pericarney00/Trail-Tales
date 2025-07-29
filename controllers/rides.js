164b60420e988f451a2f1e7cf8b9641756461cc9
pericarney00 <pericarney@gmail.com>
2025-07-29 11:12:54 -0500

    Created index.ejs view page for rides

--- controllers/rides.js
+++ controllers/rides.js
@@ -3,7 +3,18 @@ const router = express.Router();
 
 const Ride = require("../models/ride");
 
-
+router.get("/", async (req, res) => {
+  try {
+    const populatedRides = await Ride.find({}).populate("owner")
+    console.log(`populated rides:${populatedRides}`)
+    res.render("rides/index.ejs", {
+      rides: populatedRides
+    })
+  } catch (error) {
+    console.log(error)
+    res.redirect("/")
+  }
+})
 
 
 module.exports = router;
